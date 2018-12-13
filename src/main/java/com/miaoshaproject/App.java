0db72780682b28b9bfac7d358dcd4af232cc72bb
PerfySchu <811601609@qq.com>
2018-12-13 15:53:26 +0800

    first commit by IDEA

---
+++ src/main/java/com/miaoshaproject/App.java
@@ -0,0 +1,28 @@
+package com.miaoshaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ * EnableAutoConfiguration 启动内嵌的tomcat并添加默认的配置
+ *
+ */
+@EnableAutoConfiguration
+@RestController
+public class App 
+{
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World";
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
