a97dcf8536fc3ef286b2db8b4e0c8023f2d628df
PerfySchu <811601609@qq.com>
2018-12-19 00:04:47 +0800

    update sql file

--- miaosha.sql
+++ miaosha.sql
@@ -1,21 +1,46 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地
+ Source Server         : My
  Source Server Type    : MySQL
- Source Server Version : 50721
+ Source Server Version : 50722
  Source Host           : localhost:3306
  Source Schema         : miaosha
 
  Target Server Type    : MySQL
- Target Server Version : 50721
+ Target Server Version : 50722
  File Encoding         : 65001
 
- Date: 18/12/2018 15:27:20
+ Date: 19/12/2018 00:03:30
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for item
+-- ----------------------------
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `price` double(10, 2) UNSIGNED ZEROFILL NOT NULL DEFAULT 0000000.00,
+  `description` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `sales` int(10) UNSIGNED ZEROFILL NOT NULL DEFAULT 0000000000,
+  `img_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for item_stock
+-- ----------------------------
+DROP TABLE IF EXISTS `item_stock`;
+CREATE TABLE `item_stock`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stock` int(10) UNSIGNED ZEROFILL NOT NULL DEFAULT 0000000000,
+  `item_id` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user_info
@@ -31,7 +56,12 @@ CREATE TABLE `user_info`  (
   `third_party_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `telephone_unique_index`(`telephone`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_info
+-- ----------------------------
+INSERT INTO `user_info` VALUES (14, '苏凝汐', 1, 23, '15527186096', 'byphone', '');
 
 -- ----------------------------
 -- Table structure for user_password
@@ -42,6 +72,11 @@ CREATE TABLE `user_password`  (
   `encrypt_password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `user_id` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_password
+-- ----------------------------
+INSERT INTO `user_password` VALUES (13, '4QrcOUm6Wau+VuBX8g+IPg==', 14);
 
 SET FOREIGN_KEY_CHECKS = 1;
