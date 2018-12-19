73b3f72e5de783864a8ddf5e5f9a115fbf3963b0
PerfySchu <811601609@qq.com>
2018-12-19 18:06:26 +0800

    item VO service and impelement

---
+++ src/main/java/com/miaoshaproject/service/ItemService.java
@@ -0,0 +1,24 @@
+package com.miaoshaproject.service;
+
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.service.model.ItemModel;
+
+import java.util.List;
+
+/**
+ * @ClassName ItemService
+ * @Author PerfySchu
+ * @Date 2018/12/19 12:57
+ * @Version 1.0
+ **/
+public interface ItemService {
+
+    //创建商品
+    ItemModel createItem(ItemModel itemModel) throws BusinessException;
+
+    //商品列表浏览
+    List<ItemModel> listItem();
+
+    //商品详情浏览
+    ItemModel getItemById(Integer id);
+}
